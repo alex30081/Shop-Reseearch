f52f30d3b5467a5bcb6bd99d92d3d232279191bc
Boichenko Oleksandr <alex.homerepair.www@gmail.com>
2024-06-04 19:44:51 +0100

    Fix bugs class DataHandler

---
+++ src/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DataProvider provider = new DataProvider();
+        DataHandler dataHandler = new DataHandler();
+
+        String namesOutput;
+        String salesOutput;
+
+        namesOutput = dataHandler.handleData(provider.getProductNames());
+        getOutput("Products: " + namesOutput);
+
+
+        salesOutput = dataHandler.handleData(provider.getSalesAmounts());
+        getOutput("Sales, EUR: " + salesOutput);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
+}
+
